ed14f53d3f30e4b3624782199524fad0c9cf5285
Chris Campo <ccampo.progs@gmail.com>
2014-07-03 00:15:36 -0400

    Switched to Velocity as a view engine.

--- src/main/java/ccampo133/redditvoicemail/Application.java
+++ src/main/java/ccampo133/redditvoicemail/Application.java
@@ -2,26 +2,25 @@ package ccampo133.redditvoicemail;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.context.web.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @ComponentScan
 @EnableAutoConfiguration
-public class Application extends SpringBootServletInitializer {
+public class Application {// extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
-    
-    
+
+    /*
     @Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+    protected SpringApplicationBuilder configure(
+            SpringApplicationBuilder application) {
         return application.sources(applicationClass);
     }
 
     private static Class<Application> applicationClass = Application.class;
-    
+    */
 }
--- src/main/java/ccampo133/redditvoicemail/controllers/MainController.java
+++ src/main/java/ccampo133/redditvoicemail/controllers/MainController.java
@@ -1,14 +1,21 @@
 package ccampo133.redditvoicemail.controllers;
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class MainController {
-
-	@RequestMapping("/index.xml")
-	public String index() {
-		return "index";
-	}
+    
+    @RequestMapping(value = "/main.xml", method = RequestMethod.GET, produces = {"application/xml"})
+    public String main(Model model) {
+        Calendar cal = Calendar.getInstance();
+        SimpleDateFormat sdf = new SimpleDateFormat("HH:mm:ss");
+        model.addAttribute("time", sdf.format(cal.getTime()));
+        return "main";
+    }
 }
-
